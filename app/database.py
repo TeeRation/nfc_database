58451cc44c8170bed4a961c36025748a45e054dd
TeeRation <180157973+TeeRation@users.noreply.github.com>
2026-07-27 13:39:44 +0300

    Оформил абсолютный путь, для упрощения поиска (не уверен в необходимости)

--- app/database.py
+++ app/database.py
@@ -1,7 +1,12 @@
+from pathlib import Path
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "sqlite:///./nfc_database.db"
+BASE_DIR = Path(__file__).resolve().parent.parent
+DATABASE_PATH = BASE_DIR / "database" / "nfc_database.db"
+
+DATABASE_URL = f"sqlite:///{DATABASE_PATH}"
 
 engine = create_engine(
     DATABASE_URL,
